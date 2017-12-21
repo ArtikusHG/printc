362029b10b2696d2869fbec1ab847b77256f8f6f
ArtikusHG <artiomoleynic@gmail.com>
2017-12-21 17:34:06 +0200

    Initial commit

--- printc.c
+++ printc.c
@@ -4,9 +4,10 @@ int main(int argc, char **argv) {
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
     if(argc < 2) {
-    printf("Usage: printc \"Hello, world!\"\nWarning: this only works great with small words. Bigger words might not be properly centered.\n");
+    printf("Usage: printc \"Hello, world!\"\n");
     } else {
-    printf("%*s\n", w.ws_col/2, argv[1]);
+    int sp = (strlen(argv[1]) + w.ws_col)/2;
+    printf("%*s\n", sp, argv[1]);
     return 0;
     }
 }
